0eeb68c5984ff6c0811548f9169c9c9e69c12c84
ikcilrep <frajerzycki@protonmail.com>
2020-10-21 19:56:10 +0200

    Fix label

--- src/Components/UserPersonalData.js
+++ src/Components/UserPersonalData.js
@@ -5,27 +5,27 @@ import ReactDOM from 'react-dom';
 class UserPersonalData extends React.Component {
   render() {
     return <div>
-       <form>
-  <label>
-    Imię:
+      <form>
+        <label>
+          Imię:
     <input type="text" name="name" />
     Nazwisko:
     <input type="text" name="surname" />
-    Data urodzenia:
+    Wiek:
     <input type="number" name="age" />
     Płeć:
     <select>
-  <option value="Men">Mężczyzna</option>
-  <option value="Woman">Kobieta</option>
-  <option value="Other">Inny</option>
-</select>
-  </label>
-  <input type="submit" value="Wyślij" />
-</form> 
+            <option value="Men">Mężczyzna</option>
+            <option value="Woman">Kobieta</option>
+            <option value="Other">Inny</option>
+          </select>
+        </label>
+        <input type="submit" value="Wyślij" />
+      </form>
 
     </div>;
-   
-}
+
+  }
 }
 
 export default UserPersonalData;
