8b65d123e176f145cdd0dba4210ab95bcb991e6e
ofikasty <32196463+ofikasty@users.noreply.github.com>
2020-10-21 19:44:29 +0200

    UserForm
    form for user data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import UserPersonalData from './Components/UserPersonalData.js';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <UserPersonalData />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/Components/UserPersonalData.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class UserPersonalData extends React.Component {
+  render() {
+    return <div>
+       <form>
+  <label>
+    Imię:
+    <input type="text" name="name" />
+    Nazwisko:
+    <input type="text" name="surname" />
+    Data urodzenia:
+    <input type="number" name="age" />
+    Płeć:
+    <select>
+  <option value="Men">Mężczyzna</option>
+  <option value="Woman">Kobieta</option>
+  <option value="Other">Inny</option>
+</select>
+  </label>
+  <input type="submit" value="Wyślij" />
+</form> 
+
+    </div>;
+   
+}
+}
+
+export default UserPersonalData;
