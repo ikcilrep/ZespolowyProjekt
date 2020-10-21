d75948a41caca34b034b931f07eaac495e3e4b82
ikcilrep <frajerzycki@protonmail.com>
2020-10-21 19:32:31 +0200

    Use button group

--- src/Components/LanguageChoice.js
+++ src/Components/LanguageChoice.js
@@ -1,4 +1,5 @@
 import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 import React from 'react';
 
 const POLISH = 'Polski';
@@ -7,9 +8,15 @@ const SPANISH = 'Español';
 
 const LanguageChoice = ({ onLanguageChosen }) => {
     return (<div>
-        <Button onClick={() => onLanguageChosen(POLISH)}>{POLISH}</Button>
-        <Button onClick={() => onLanguageChosen(ENGLISH)}>{ENGLISH}</Button>
-        <Button onClick={() => onLanguageChosen(SPANISH)}>{SPANISH}</Button>
+        <ButtonGroup size="large"
+            variant="contained"
+            color="primary"
+            aria-label="contained primary button group">
+            <Button onClick={() => onLanguageChosen(POLISH)}>{POLISH}</Button>
+            <Button onClick={() => onLanguageChosen(ENGLISH)}>{ENGLISH}</Button>
+            <Button onClick={() => onLanguageChosen(SPANISH)}>{SPANISH}</Button>
+        </ButtonGroup>
+
     </div>);
 };
 
