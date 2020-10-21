6c44ac9eafa4ecc80165efae6b337bc0264988f5
ikcilrep <frajerzycki@protonmail.com>
2020-10-21 19:43:04 +0200

    Center button group

--- src/Components/LanguageChoice.js
+++ src/Components/LanguageChoice.js
@@ -1,6 +1,7 @@
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 import React from 'react';
+import './LanguageChoice.css';
 
 const POLISH = 'Polski';
 const ENGLISH = 'English';
@@ -8,16 +9,17 @@ const SPANISH = 'Español';
 
 const LanguageChoice = ({ onLanguageChosen }) => {
     return (<div>
-        <ButtonGroup size="large"
-            variant="contained"
-            color="primary"
-            aria-label="contained primary button group">
-            <Button onClick={() => onLanguageChosen(POLISH)}>{POLISH}</Button>
-            <Button onClick={() => onLanguageChosen(ENGLISH)}>{ENGLISH}</Button>
-            <Button onClick={() => onLanguageChosen(SPANISH)}>{SPANISH}</Button>
-        </ButtonGroup>
-
-    </div>);
+        <div className='centered'>
+            <ButtonGroup size="large"
+                variant="contained"
+                color="primary"
+                aria-label="contained primary button group">
+                <Button onClick={() => onLanguageChosen(POLISH)}>{POLISH}</Button>
+                <Button onClick={() => onLanguageChosen(ENGLISH)}>{ENGLISH}</Button>
+                <Button onClick={() => onLanguageChosen(SPANISH)}>{SPANISH}</Button>
+            </ButtonGroup>
+        </div>
+    </div >);
 };
 
 export { POLISH, ENGLISH, SPANISH };
