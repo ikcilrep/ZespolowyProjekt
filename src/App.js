5703eefa9cc04ff9cae54c4b45edb6ba58a070b5
ikcilrep <frajerzycki@protonmail.com>
2020-10-23 21:26:16 +0200

    Add reading to router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React from "react";
 import LanguageChoice from "./Components/LanguageChoice";
 import Error404 from "./Components/Error404";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import './App.css';
+import "./App.css";
+import Reading from "./Components/Reading";
 
 class App extends React.Component {
   constructor(props) {
@@ -10,17 +11,32 @@ class App extends React.Component {
     this.state = {};
   }
 
-  choseLanguage = language => {
+  chooseLanguage = (language) => {
     this.setState({ language });
     console.log("lang set:", language);
-    document.location = "/data";
-  }
+    document.location = "/reading";
+  };
+
+  onRead = () => {};
 
   render() {
+    const text =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
     return (
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={() => <LanguageChoice onLanguageChosen={this.choseLanguage} />} />
+          <Route
+            exact
+            path="/"
+            component={() => (
+              <LanguageChoice onLanguageChosen={this.chooseLanguage} />
+            )}
+          />
+          <Route
+            path="/reading"
+            component={() => <Reading text={text} onRead={this.onRead} />}
+          />
           <Route component={Error404} />
         </Switch>
       </BrowserRouter>
