6164177bbd071caa003dfe7b87531c7d8aac1c71
ikcilrep <frajerzycki@protonmail.com>
2020-10-25 12:58:15 +0100

    Turn App into function component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import LanguageChoice, {
   POLISH,
   ENGLISH,
@@ -10,66 +10,67 @@ import "./App.css";
 import Reading from "./Components/Reading";
 import Instruction from "./Components/Instruction";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { language: ENGLISH, redirect: false };
-  }
+const getInstruction = () => {
+  const instruction = {};
+  instruction[POLISH] = ["Zrób coś", "Zrób coś innego"];
+  instruction[ENGLISH] = ["Do something", "Do something different"];
+  instruction[SPANISH] = ["Hacer algo", "Hacer algo más"];
+  return instruction;
+};
 
-  chooseLanguage = (language) => {
-    this.setState({ language, redirect: true });
+const App = () => {
+  const [language, setLanguage] = useState(ENGLISH);
+  const [redirect, setRedirect] = useState(false);
+
+  const chooseLanguage = (language) => {
+    setLanguage(language);
+    setRedirect(true);
   };
 
-  onReadText = () => {};
-  onReadInstruction = () => {
-    this.setState({ redirect: true });
+  const onReadText = () => {};
+  const onReadInstruction = () => {
+    setRedirect(true);
   };
+  const text =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
-  render() {
-    const text =
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+  const instruction = getInstruction();
 
-    const instruction = {};
-    instruction[POLISH] = ["Zrób coś", "Zrób coś innego"];
-    instruction[ENGLISH] = ["Do something", "Do something different"];
-    instruction[SPANISH] = ["Hacer algo", "Hacer algo más"];
-
-    return (
-      <BrowserRouter>
-        <Switch>
-          <Route
-            exact
-            path="/"
-            component={() => (
-              <LanguageChoice
-                onLanguageChosen={this.chooseLanguage}
-                nextPagePath="/instruction"
-                onRedirect={() => this.setState({ redirect: false })}
-                redirect={this.state.redirect}
-              />
-            )}
-          />
-          <Route
-            path="/instruction"
-            component={() => (
-              <Instruction
-                points={instruction[this.state.language]}
-                onRead={this.onReadInstruction}
-                nextPagePath="/reading"
-                onRedirect={() => this.setState({ redirect: false })}
-                redirect={this.state.redirect}
-              />
-            )}
-          />
-          <Route
-            path="/reading"
-            component={() => <Reading text={text} onRead={this.onReadText} />}
-          />
-          <Route component={Error404} />
-        </Switch>
-      </BrowserRouter>
-    );
-  }
-}
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          component={() => (
+            <LanguageChoice
+              onLanguageChosen={chooseLanguage}
+              nextPagePath="/instruction"
+              onRedirect={() => setRedirect(false)}
+              redirect={redirect}
+            />
+          )}
+        />
+        <Route
+          path="/instruction"
+          component={() => (
+            <Instruction
+              points={instruction[language]}
+              onRead={onReadInstruction}
+              nextPagePath="/reading"
+              onRedirect={() => setRedirect(false)}
+              redirect={redirect}
+            />
+          )}
+        />
+        <Route
+          path="/reading"
+          component={() => <Reading text={text} onRead={onReadText} />}
+        />
+        <Route component={Error404} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 export default App;
