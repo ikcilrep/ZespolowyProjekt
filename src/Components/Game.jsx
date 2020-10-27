94ea6c3725976f4b5ff5fffd05958b8705a09f11
ikcilrep <frajerzycki@protonmail.com>
2020-10-27 13:59:24 +0100

    Add Baloon to Game

--- src/Components/Game.jsx
+++ src/Components/Game.jsx
@@ -1,5 +1,23 @@
-const Game = () => {
+import React, { useState } from 'react';
+import Baloon from './Baloon';
 
+const Game = () => {
+    const [prize, setPrize] = useState(0);
+
+    const MONEY_FOR_PUMP = 5;
+
+    const onSuccessfulPump = () => {
+        setPrize(prize + MONEY_FOR_PUMP);
+    };
+
+    const onExplosion = () => {
+        setPrize(0);
+    };
+
+    return <div>
+        <h1>Prize: {prize}$</h1>
+        <Baloon onSuccessfulPump={onSuccessfulPump} onExplosion={onExplosion} />
+    </div>;
 };
 
 export default Game;
