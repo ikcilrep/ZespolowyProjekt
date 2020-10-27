6ec2ff166218836bcf3d902391500a4de7c35ff4
ikcilrep <frajerzycki@protonmail.com>
2020-10-27 14:04:47 +0100

    Add handling explosion and pumps

--- src/Components/Baloon.jsx
+++ src/Components/Baloon.jsx
@@ -1,22 +1,37 @@
 import React from "react";
 import "./Baloon.css";
 
-export default function Baloon() {
+export default function Baloon({ onSuccessfulPump, onExplosion }) {
   const [pump, setPump] = React.useState(0);
   const [pumps, setPumps] = React.useState(0);
+  const handleClick = () => {
+    setPump(1);
+    if (pumps % 5 === 4) {
+      onExplosion();
+    } else {
+      onSuccessfulPump();
+    }
+    setPumps(pumps + 1);
+
+  };
+
+  const handleAnimationEnd = () => {
+    setPump(0);
+  };
+
   return (
     <div>
-      <div class="container" onClick={() => { setPump(1); setPumps(pumps + 1); }} onAnimationEnd={() => setPump(0)} pump={pump} pumps={pumps}>
-        <div class="ground" pump={pump}></div>
-        <div class="hose" pump={pump}></div>
-        <div class="piston" pump={pump}>
-          <div class="stick1" pump={pump}></div>
-          <div class="stick2" pump={pump}></div>
-          <div class="box" pump={pump}></div>
-          <div class="stamp-balloon" pump={pump}></div>
-          <div class="stamp-triangle" pump={pump}></div>
+      <div className="container" onClick={handleClick} onAnimationEnd={handleAnimationEnd} pump={pump} pumps={pumps}>
+        <div className="ground" pump={pump}></div>
+        <div className="hose" pump={pump}></div>
+        <div className="piston" pump={pump}>
+          <div className="stick1" pump={pump}></div>
+          <div className="stick2" pump={pump}></div>
+          <div className="box" pump={pump}></div>
+          <div className="stamp-balloon" pump={pump}></div>
+          <div className="stamp-triangle" pump={pump}></div>
         </div>
-        <div class="baloon" pump={pump} pumps={pumps}></div>
+        <div className="baloon" pump={pump} pumps={pumps}></div>
       </div>
     </div>
   );
