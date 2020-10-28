6d3b01bca928a47bc94b97edb067b58c2cadca6d
Patryk Popowicz <ptrykppowicz@gmail.com>
2020-10-28 18:22:25 +0100

    changed css
    > move UserPersonalData to specific css file
    > change quiz style

--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -1,102 +1,118 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
 import "./Quiz.css";
-import { Button} from "@material-ui/core";
+import { Button } from "@material-ui/core";
 
-export default function Quiz({nextPagePath,language}) {
-	const questions = [
-		{
-			questionText: 'What is the capital of France?',
-			answerOptions: [
-				{ answerText: 'New York', isCorrect: false },
-				{ answerText: 'London', isCorrect: false },
-				{ answerText: 'Paris', isCorrect: true },
-				{ answerText: 'Dublin', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'Who is CEO of Tesla?',
-			answerOptions: [
-				{ answerText: 'Jeff Bezos', isCorrect: false },
-				{ answerText: 'Elon Musk', isCorrect: true },
-				{ answerText: 'Bill Gates', isCorrect: false },
-				{ answerText: 'Tony Stark', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'The iPhone was created by which company?',
-			answerOptions: [
-				{ answerText: 'Apple', isCorrect: true },
-				{ answerText: 'Intel', isCorrect: false },
-				{ answerText: 'Amazon', isCorrect: false },
-				{ answerText: 'Microsoft', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'How many Harry Potter books are there?',
-			answerOptions: [
-				{ answerText: '1', isCorrect: false },
-				{ answerText: '4', isCorrect: false },
-				{ answerText: '6', isCorrect: false },
-				{ answerText: '7', isCorrect: true },
-			],
-		},
-		{
-			questionText: 'Will this work?',
-			answerOptions: [
-				{ answerText: 'Yes', isCorrect: false },
-				{ answerText: 'No', isCorrect: false },
-				{ answerText: 'Maybe', isCorrect: false },
-				{ answerText: 'All answers are correct', isCorrect: true },
-			],
-		},
-	];
-  	
+export default function Quiz({ nextPagePath, language }) {
+  const questions = [
+    {
+      questionText: "What is the capital of France?",
+      answerOptions: [
+        { answerText: "New York", isCorrect: false },
+        { answerText: "London", isCorrect: false },
+        { answerText: "Paris", isCorrect: true },
+        { answerText: "Dublin", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "Who is CEO of Tesla?",
+      answerOptions: [
+        { answerText: "Jeff Bezos", isCorrect: false },
+        { answerText: "Elon Musk", isCorrect: true },
+        { answerText: "Bill Gates", isCorrect: false },
+        { answerText: "Tony Stark", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "The iPhone was created by which company?",
+      answerOptions: [
+        { answerText: "Apple", isCorrect: true },
+        { answerText: "Intel", isCorrect: false },
+        { answerText: "Amazon", isCorrect: false },
+        { answerText: "Microsoft", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "How many Harry Potter books are there?",
+      answerOptions: [
+        { answerText: "1", isCorrect: false },
+        { answerText: "4", isCorrect: false },
+        { answerText: "6", isCorrect: false },
+        { answerText: "7", isCorrect: true },
+      ],
+    },
+    {
+      questionText: "Will this work?",
+      answerOptions: [
+        { answerText: "Yes", isCorrect: false },
+        { answerText: "No", isCorrect: false },
+        { answerText: "Maybe", isCorrect: false },
+        { answerText: "All answers are correct", isCorrect: true },
+      ],
+    },
+  ];
 
-	const [currentQuestion, setCurrentQuestion] = useState(0);
-	const [showScore, setShowScore] = useState(false);
-	const [redirect, setRedirect] = useState(false);
-	const [score, setScore] = useState(0);
-	
-	if (redirect) {
-    	return <Redirect to={nextPagePath} />;
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [redirect, setRedirect] = useState(false);
+  const [score, setScore] = useState(0);
+
+  if (redirect) {
+    return <Redirect to={nextPagePath} />;
   }
-	const handleAnswerOptionClick = (isCorrect) => {
-		if (isCorrect) {
-			setScore(score + 1);
-		}
+  const handleAnswerOptionClick = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
 
-		const nextQuestion = currentQuestion + 1;
-		if (nextQuestion < questions.length) {
-			setCurrentQuestion(nextQuestion);
-		} else {
-			setShowScore(true);
-		}
-	};
-	return (
-		<div className='quiz'>
-		<div className='app'>
-			{showScore ? (
-				<div className='score-section'>
-					You scored {score} out of {questions.length}
-					<Button onClick={() => setRedirect(true) } variant="contained" color="secondary">OK</Button>
-				</div>
-			) : (
-				<>
-					<div className='question-section'>
-						<div className='question-count'>
-							<span>Question {currentQuestion + 1}</span>/{questions.length}
-						</div>
-						<div className='question-text'>{questions[currentQuestion].questionText}</div>
-					</div>
-					<div className='answer-section'>
-						{questions[currentQuestion].answerOptions.map((answerOption) => (
-							<Button onClick={() => handleAnswerOptionClick(answerOption.isCorrect) } variant="contained" color="secondary">{answerOption.answerText}</Button>
-						))}
-					</div>
-				</>
-			)}
-		</div>
-		</div>
-	);
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
+  return (
+    <div className="quiz">
+      <div className="app">
+        {showScore ? (
+          <div className="score-section">
+            You scored {score} out of {questions.length}
+            <Button
+              onClick={() => setRedirect(true)}
+              variant="contained"
+              color="secondary"
+              id="confirmbutton"
+            >
+              OK
+            </Button>
+          </div>
+        ) : (
+          <>
+            <div className="question-section">
+              <div className="question-count">
+                <span>Question {currentQuestion + 1}</span>/{questions.length}
+              </div>
+              <div className="question-text">
+                {questions[currentQuestion].questionText}
+              </div>
+            </div>
+            <div className="answer-section">
+              {questions[currentQuestion].answerOptions.map((answerOption) => (
+                <Button
+                  onClick={() =>
+                    handleAnswerOptionClick(answerOption.isCorrect)
+                  }
+                  variant="contained"
+                  color="secondary"
+                >
+                  {answerOption.answerText}
+                </Button>
+              ))}
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
