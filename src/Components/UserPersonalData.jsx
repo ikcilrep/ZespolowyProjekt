c00cedb876e08fe2bc40843c709920d9897847c3
Patryk Popowicz <ptrykppowicz@gmail.com>
2020-10-28 18:17:54 +0100

    UserPersonalData style change

--- src/Components/UserPersonalData.jsx
+++ src/Components/UserPersonalData.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Redirect } from "react-router-dom";
 import { useState } from "react";
-import { ENGLISH, POLISH, SPANISH } from './LanguageChoice';
+import { ENGLISH, POLISH, SPANISH } from "./LanguageChoice";
 import Button from "@material-ui/core/Button";
 import "./UserPersonalData.css";
 
-const MAN = 'Man';
-const WOMAN = 'Woman';
-const OTHER = 'Other';
+const MAN = "Man";
+const WOMAN = "Woman";
+const OTHER = "Other";
 
 const UserPersonalData = ({ onDataEntered, nextPagePath, language }) => {
   const [firstName, setFirstName] = useState("");
@@ -18,32 +18,32 @@ const UserPersonalData = ({ onDataEntered, nextPagePath, language }) => {
 
   const dictionary = {};
   dictionary[ENGLISH] = {
-    Man: 'Man',
-    Woman: 'Woman',
-    Other: 'Other',
-    FirstName: 'First name',
-    LastName: 'Last name',
-    Age: 'Age',
-    Sex: 'Sex'
+    Man: "Man",
+    Woman: "Woman",
+    Other: "Other",
+    FirstName: "First name",
+    LastName: "Last name",
+    Age: "Age",
+    Sex: "Sex",
   };
   dictionary[POLISH] = {
-    Man: 'Mężczyzna',
-    Woman: 'Kobieta',
-    Other: 'Inny',
-    FirstName: 'Imię',
-    LastName: 'Nazwisko',
-    Age: 'Wiek',
-    Sex: 'Płeć'
+    Man: "Mężczyzna",
+    Woman: "Kobieta",
+    Other: "Inny",
+    FirstName: "Imię",
+    LastName: "Nazwisko",
+    Age: "Wiek",
+    Sex: "Płeć",
   };
 
   dictionary[SPANISH] = {
-    Man: 'Hombre',
-    Woman: 'Mujer',
-    Other: 'Diferente',
-    FirstName: 'Nombre',
-    LastName: 'Apellido',
-    Age: 'Años',
-    Sex: 'Sexo'
+    Man: "Hombre",
+    Woman: "Mujer",
+    Other: "Diferente",
+    FirstName: "Nombre",
+    LastName: "Apellido",
+    Age: "Años",
+    Sex: "Sexo",
   };
 
   const handleClick = () => {
@@ -56,41 +56,38 @@ const UserPersonalData = ({ onDataEntered, nextPagePath, language }) => {
   }
 
   return (
-    <div style={{ color: "black" }}>
-      <label>
-        {dictionary[language].FirstName}:
-        <input
-          onChange={(e) => setFirstName(e.target.value)}
-          type="text"
-          name="name"
-        />
-        {dictionary[language].LastName}:
-        <input
-          onChange={(e) => setLastName(e.target.value)}
-          type="text"
-          name="surname"
-        />
-
-        {dictionary[language].Age}:
-        <input
-          onChange={(e) => setAge(e.target.value)}
-          type="number"
-          name="age"
-        />
-        {dictionary[language].Sex}:
-        <select onChange={(e) => setSex(e.target.value)}>
-          <option value={MAN}>{dictionary[language].Man}</option>
-          <option value={WOMAN}>{dictionary[language].Woman}</option>
-          <option value={OTHER}>{dictionary[language].Other}</option>
-        </select>
-      </label>
-      <Button
-        onClick={handleClick}
-        variant="contained"
-        color="secondary"
-      >
-        Ok
-      </Button>
+    <div style={{ color: "black" }} id="personaldataform" className="centered">
+      <div style={{ width: "75%" }}>
+        <label>
+          {dictionary[language].FirstName}:
+          <input
+            onChange={(e) => setFirstName(e.target.value)}
+            type="text"
+            name="name"
+          />
+          {dictionary[language].LastName}:
+          <input
+            onChange={(e) => setLastName(e.target.value)}
+            type="text"
+            name="surname"
+          />
+          {dictionary[language].Age}:
+          <input
+            onChange={(e) => setAge(e.target.value)}
+            type="number"
+            name="age"
+          />
+          {dictionary[language].Sex}:
+          <select onChange={(e) => setSex(e.target.value)}>
+            <option value={MAN}>{dictionary[language].Man}</option>
+            <option value={WOMAN}>{dictionary[language].Woman}</option>
+            <option value={OTHER}>{dictionary[language].Other}</option>
+          </select>
+        </label>
+        <Button onClick={handleClick} variant="contained" color="secondary">
+          Ok
+        </Button>
+      </div>
     </div>
   );
 };
